3a6f490210c0c43b5d383c5adfe2bb0c93d886d3
Magzhan-bit <dijimaga@gmail.com>
2023-12-04 15:20:25 +0600

    first commit

---
+++ resources/views/generate.blade.php
@@ -0,0 +1,16 @@
+<?php
+function generateRandomPassword($length = 8) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_=+';
+    $password = '';
+
+    $charactersLength = strlen($characters);
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $characters[rand(0, $charactersLength - 1)];
+    }
+
+    return $password;
+}
+
+$password = generateRandomPassword(8);
+echo "Случайный пароль: " . $password;
+?>
